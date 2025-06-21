c261146c9e709a9bb600320567e7c7bc1dab7111
Tukis-bit <arthur01raphael@gmail.com>
2025-06-21 00:46:17 -0300

    op 1 completa

--- Vendas/app.js
+++ Vendas/app.js
@@ -11,9 +11,15 @@ for(let i = 1; i != 0; i++){
     funcoes.apresentarMenu();
     let resposta = Number(ler());
 
+    //faz o registro da venda 
     if(resposta == 1){
 
+        console.log('\nQUal foi a quantidade de vendas feita?');
+        let quant = Number(ler());
+        
+        for(let i = 0; i <= quant; i ++ ){
         funcoes.registrarVenda(vendas);
+    }
 
     }
 }
